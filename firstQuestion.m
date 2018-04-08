033bebcb2c483c598e95b5c754a5c1c45ee43b33
Nilson <nlj@cin.ufpe.br>
2018-04-08 02:40:48 -0300

    using subplot

--- firstQuestion.m
+++ firstQuestion.m
@@ -1,13 +1,29 @@
 function firstQuestion
-delta = 0.1 * pi
+delta = 0.08 * pi
 wc1 = 0.3 * pi
 wc2 = 0.4 * pi
 wc3 = 0.5 * pi
 wc4 = 0.6 * pi
 wc5 = 1.0 * pi
-M = ceil((6.6 * pi)/delta) + 1
+M = ceil((6.6 * pi)/delta) + 10
+
 wHamming = (hamming(M))'
 hLowPass = idealLowPass(wc5, M) - idealLowPass(wc4, M) + idealLowPass(wc1, M) + idealLowPass(wc3, M) - idealLowPass(wc2, M)
 h = hLowPass .* wHamming
-[db, mag, phase, w] = freqzModified(h, [1])
-stem(mag)
+
+[db] = freqzModified(h, [1])
+
+figure
+
+ax1 = subplot(2, 1, 1)
+stem(h)
+title(ax1, 'Impulso')
+ylabel(ax1, 'h[n]')
+
+ax2 = subplot(2, 1, 2)
+plot(ax2, db)
+title(ax2, 'Magnitude em dB')
+ylabel(ax2, 'Decibeis')
+grid on
+
+M
