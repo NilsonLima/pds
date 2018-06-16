9fa8430d4f60c7a2e0b2f04f0affa1a55c6df114
NilsonLima <nlj@cin.ufpe.br>
2018-06-15 22:46:51 -0300

    fourth_2 question

---
+++ projeto/fourth/fourth_2.m
@@ -0,0 +1,36 @@
+[y, Fs] = audioread('teste_de_som.wav');
+fprintf('Taxa de amostragem: %d\n', Fs);
+
+f = 466.16
+dt = 1/Fs;
+t = 0 : dt : (length(y)/Fs) - dt;
+nsin = sin(2 * pi * f * t);
+ynoise = (y' * 30) + nsin;
+
+figure
+plot(t, y)
+title('Original')
+xlabel('Seconds')
+ylabel('Amplitude');
+
+figure
+plot(t, ynoise);
+title('Noisy');
+xlabel('Seconds');
+ylabel('Amplitude');
+
+%[b a] = butter(8, 0.06);
+%[b a] = cheby1(5, 0.5, 0.06);
+%yfilt = filter(b, a, ynoise);
+
+b = fir1(50, 0.06, bartlett(51));
+yfilt = filter(b, 1, ynoise);
+
+figure
+plot(t, yfilt);
+title('Filtered');
+xlabel('Seconds');
+ylabel('Amplitude');
+
+sound(yfilt', Fs);
+pause(length(yfilt)/Fs);
